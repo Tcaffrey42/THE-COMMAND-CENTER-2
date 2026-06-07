bb862dfabd869f29a9baf66500a81864f20ada04
Tcaffrey42 <tcaffrey42@gmail.com>
2026-06-07 00:29:03 -0400

    Update env.js

--- env.js
+++ env.js
@@ -2,3 +2,12 @@ window.COMMANDCENTER_ENV = {
   SUPABASE_URL: "",
   SUPABASE_ANON_KEY: ""
 };
+window.appSignIn = appSignIn;
+window.appSignOut = appSignOut;
+window.setClient = setClient;
+window.setLoginUser = setLoginUser;
+window.render = render;
+window.openWOForm = openWOForm;
+window.openProposalForm = openProposalForm;
+window.exportData = exportData;
+window.closeModal = closeModal;
